5907cca52bdbb1d8b444075225b637e49c815fd9
DFuen <dfuemar606@g.educaand.es>
2025-11-27 21:41:56 +0100

    TERMINADO EJERCICIO 6

--- Tema_4/6/main.js
+++ Tema_4/6/main.js
@@ -1,14 +1,26 @@
 /*CONTENEDOR DONDE CREAR LAS TARJETAS*/
-const contendor=document.getElementById("tarjetasCreadas");
+const contenedor=document.getElementById("tarjetasCreadas");
 /*ELEMENTOS DE LAS TARJETAS*/
-let titulo=document.getElementById("titulo");
-let imagen=document.getElementById("imagen");
-let url=document.getElementById("url");
-let texto=document.getElementById("texto");
 
 /*CREAR ESCUCHADOR CLICK*/
 document.getElementById("crear").addEventListener("click",crearTarjeta);
 
 function crearTarjeta(){
-    
+    let titulo=document.getElementById("titulo");
+    let url=document.getElementById("url");
+    let texto=document.getElementById("texto");
+    let valorTitulo = titulo.value;
+    let valorUrl = url.value;
+    let valorTexto = texto.value;
+
+    let contenedorBase =`
+        <div id="content-imagen">
+            <div class="imagenes">
+                <img src="${valorUrl}">
+                <span class="nombre">${valorTitulo}</span>
+                <span class="nombre">${valorTexto}</span>
+            </div>
+        </div>
+    `;
+    contenedor.innerHTML+=contenedorBase;
 }
