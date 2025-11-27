93cd98eb164461df52fd5b8d6f34f27e57d5798d
DFuen <dfuemar606@g.educaand.es>
2025-11-27 21:03:44 +0100

    EJERCICIO 6 SIN ACABAR

---
+++ Tema_4/6/main.js
@@ -0,0 +1,14 @@
+/*CONTENEDOR DONDE CREAR LAS TARJETAS*/
+const contendor=document.getElementById("tarjetasCreadas");
+/*ELEMENTOS DE LAS TARJETAS*/
+let titulo=document.getElementById("titulo");
+let imagen=document.getElementById("imagen");
+let url=document.getElementById("url");
+let texto=document.getElementById("texto");
+
+/*CREAR ESCUCHADOR CLICK*/
+document.getElementById("crear").addEventListener("click",crearTarjeta);
+
+function crearTarjeta(){
+    
+}
